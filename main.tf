7222faf07c3b74b3f7c060679909df123e088870
Sam Lau <esayemm@gmail.com>
2016-10-08 13:30:34 -0700

    output etcd private ips

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 variable "do_token" {}
 variable "ssh_fingerprint" {}
 variable "etcd_count" {}
+variable "kube_controller_count" {}
 
 provider "digitalocean" {
   token = "${var.do_token}"
@@ -13,6 +14,21 @@ module "etcd" {
   ssh_fingerprint = "${var.ssh_fingerprint}"
 }
 
+module "kube_controller" {
+  source = "./modules/kube_controller"
+
+  count = "${var.kube_controller_count}"
+  ssh_fingerprint = "${var.ssh_fingerprint}"
+}
+
 output "etcd_ips" {
   value = "${module.etcd.public_ips}"
+}
+
+output "etcd_ips_private" {
+  value = "${module.etcd.private_ips}"
+}
+
+output "kube_controller_ips" {
+  value = "${module.kube_controller.public_ips}"
 }
