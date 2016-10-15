b26ba398a998b12a14cb20d7d01ae1561e07df8a
Sam Lau <esayemm@gmail.com>
2016-10-14 19:23:10 -0700

    better script arguments

--- scripts/setup-kubtctl.sh
+++ scripts/setup-kubtctl.sh
@@ -1,23 +1,59 @@
-# ./setup-kubtctl.sh <kube controller ip> <token> <ca file>
+# ./setup-kubtctl.sh -k <kube controller ip> -t <token> -c <ca file>
 
-kube_controller_ip=https://$1:6443
-token=$2
-ca_file=$3
+usage() {
+cat << EOF
+usage: $0 options
 
-cluster_name=cluster
-context_name=$cluster_name-context
+OPTIONS:
+  -h help
+  -k kubernete controller ip
+  -t token
+  -c ca file location
+EOF
+}
 
-echo $kube_controller_ip $token $ca_file $cluster_name $context_name
+k_ip=
+token=
+ca_file=
+while getopts 'hk:t:c:' flag; do
+  case "${flag}" in
+    h)
+      usage
+      exit 1
+      ;;
+    k) k_ip="${OPTARG}" ;;
+    t) token="${OPTARG}" ;;
+    c) ca_file="${OPTARG}" ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
 
-kubectl config set-cluster $cluster_name \
-  --certificate-authority=$ca_file \
-  --embed-certs=true \
-  --server=$kube_controller_ip
+if [[ -z $k_ip ]] || [[ -z $token ]] || [[ -z $ca_file ]]
+then
+  usage
+  exit 1
+fi
 
-kubectl config set-credentials admin --token $token
+setupKubectl() {
+  kube_controller_ip=https://$k_ip:6443
 
-kubectl config set-context $context_name \
-  --cluster=$cluster_name \
-  --user=admin
+  cluster_name=cluster
+  context_name=$cluster_name-context
 
-kubectl config use-context $context_name
+  echo $kube_controller_ip $token $ca_file $cluster_name $context_name
+
+  kubectl config set-cluster $cluster_name \
+    --certificate-authority=$ca_file \
+    --embed-certs=true \
+    --server=$kube_controller_ip
+
+  kubectl config set-credentials admin --token $token
+
+  kubectl config set-context $context_name \
+    --cluster=$cluster_name \
+    --user=admin
+
+  kubectl config use-context $context_name
+}
+
+setupKubectl
